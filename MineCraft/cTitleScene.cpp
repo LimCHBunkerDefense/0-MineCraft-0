8252602915163e9c3294245c91adaa82b83ee174
윤중 김 <bellumgod@gmail.com>
2017-04-26 09:59:24 +0900

    TitleScene추가 내용은 수정중.

---
+++ MineCraft/cTitleScene.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "cTitleScene.h"
+
+
+cTitleScene::cTitleScene()
+{
+}
+
+
+cTitleScene::~cTitleScene()
+{
+}
+
+
+void cTitleScene::Setup() {}
+void cTitleScene::Update() {}
+void cTitleScene::Render() {}
---
+++ MineCraft/cTitleScene.h
@@ -0,0 +1,12 @@
+#pragma once
+class cTitleScene
+{
+public:
+	cTitleScene();
+	~cTitleScene();
+
+	void Setup();
+	void Update();
+	void Render();
+};
+
