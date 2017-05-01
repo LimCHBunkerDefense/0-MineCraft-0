1684a6540715e71da6d8c06962f096ded8807e01
chang young ju <chang343@naver.com>
2017-05-01 16:24:28 +0900

    push

--- MineCraft/cCamera.cpp
+++ MineCraft/cCamera.cpp
@@ -3,7 +3,7 @@
 
 
 cCamera::cCamera()
-	: m_vEye(0, 0, -5)
+	: m_vEye(0, 5, -5)
 	, m_vLookAt(0, 0, 0)
 	, m_vUp(0, 1, 0)
 	, m_pvTarget(NULL)
@@ -47,7 +47,7 @@ void cCamera::Update()
 
 	matR = matRX * matRY;
 
-	m_vEye = D3DXVECTOR3(0, 0, -m_fCameraDistance);
+	m_vEye = D3DXVECTOR3(0, 5, -m_fCameraDistance);
 	D3DXVec3TransformCoord(&m_vEye, &m_vEye, &matR);
 
 	if (m_pvTarget)
