d63212721274cb05ab739aa4e35b282035762d41
윤중 김 <bellumgod@gmail.com>
2017-05-02 15:01:49 +0900

    임시

--- MineCraft/cLoadingScene.cpp
+++ MineCraft/cLoadingScene.cpp
@@ -1,15 +1,10 @@
 #include "stdafx.h"
 #include "cLoadingScene.h"
-#include "cCamera.h"
 #include "cSurface.h"
 #include "cInputManager.h"
-#include "cTextureManager.h"
 
 cLoadingScene::cLoadingScene()
 	: m_pBg(NULL)
-	, m_pCamera(NULL)
-	, m_pTexture(NULL)
-	, m_point(0.0f, VIEW_HEIGHT * 0.5f, 0.0f)
 	
 {
 }
@@ -18,8 +13,6 @@ cLoadingScene::cLoadingScene()
 cLoadingScene::~cLoadingScene()
 {
 	SAFE_DELETE(m_pBg);
-	SAFE_DELETE(m_pCamera);
-
 }
 
 
@@ -28,9 +21,6 @@ void cLoadingScene::Setup()
 	m_pBg = new cSurface();
 	m_pBg->Setup(D3DXVECTOR3(-VIEW_WIDTH * 0.5F, 0.0F, 0.0F), D3DXVECTOR3(-VIEW_WIDTH * 0.5F, VIEW_HEIGHT, 0.0F),
 		D3DXVECTOR3(VIEW_WIDTH * 0.5F, VIEW_HEIGHT, 0.0F), D3DXVECTOR3(VIEW_WIDTH * 0.5F, 0.0F, 0.0F), TEXT("Image/LoadingScene/bg.png"));
-	m_pCamera = new cCamera();
-	m_pCamera->Setup(&m_point);
-	m_pCamera->SetPosition();
 }
 
 void cLoadingScene::Update()
@@ -49,9 +39,14 @@ void cLoadingScene::Render()
 
 	g_pD3DDevice->BeginScene();
 
-	m_pBg->Render();
+	//m_pBg->Render();
 
 	g_pD3DDevice->EndScene();
 
 	g_pD3DDevice->Present(NULL, NULL, NULL, NULL);
+}
+
+void cLoadingScene::WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
 }
