fa580bbf375efa83a5d456bde4a7dd89677c6f08
jihyoen lee <dlwlgus9125@naver.com>
2017-05-03 16:45:33 +0900

    텍스트 완성

--- MineCraft/cText_2D.cpp
+++ MineCraft/cText_2D.cpp
@@ -4,7 +4,8 @@
 
 cText_2D::cText_2D()
 {
-	D3DXCreateSprite(g_pD3DDevice, &Sprite);
+	m_Font = NULL;
+	m_color = D3DCOLOR_XRGB(0, 0, 0);
 }
 
 
@@ -12,15 +13,28 @@ cText_2D::~cText_2D()
 {
 }
 
-void cText_2D::Render()
+void cText_2D::Create(RECT rect, string message, int size, D3DXCOLOR color)
 {
-	int Height = 30;
-	int Width = 30;
-	LPCWSTR FontName = TEXT("Arial");
-	D3DXCreateFont(g_pD3DDevice, Height, Width, FW_NORMAL, 1, false, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, FontName, &this->Font);
-
-	LPCWSTR Text = TEXT("가나다라");
-	RECT rt = { 300,300,0,0 };
-	Font->DrawText(Sprite, Text, -1, &rt, DT_NOCLIP, D3DCOLOR_XRGB(255, 0, 0));
+	m_Rect = rect;
+	m_message = message;
+	m_size = size;
+	m_color = color;
+	D3DXCreateFont(g_pD3DDevice, m_size, 0, FW_NORMAL, 1, false, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS,
+		ANTIALIASED_QUALITY, FF_DONTCARE, TEXT("Arial"), &m_Font);
 
 }
+void cText_2D::Delete(string message)
+{
+}
+
+void cText_2D::Clear()
+{
+	m_Font->Release();
+	m_Font = NULL;
+}
+void cText_2D::Render()
+{
+	if(m_Font)m_Font->DrawTextA(NULL, m_message.c_str(), -1, &m_Rect, DT_LEFT, m_color);
+}
+
+
